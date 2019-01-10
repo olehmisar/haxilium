00958c454b74f495fbe36fcb8c4ca2fadbd604e6
olegmisar <olegmisar@gmail.com>
2019-01-10 21:10:22 +0200

    Add DelegatedRoom class

---
+++ src-ts/DelegatedRoom.ts
@@ -0,0 +1,92 @@
+import { NativePlayer } from './interfaces/NativePlayer';
+import { RoomConfig } from './interfaces/RoomConfig';
+import { Scores } from './interfaces/Scores';
+import { TeamID } from './interfaces/TeamID';
+import { Vector } from './interfaces/Vector';
+import { Player } from './models/Player';
+
+
+export abstract class DelegatedRoom {
+    private room: any
+    private players: { [id: number]: Player } = {}
+
+    constructor(config: RoomConfig) {
+        this.room = window.HBInit(config)
+    }
+
+    protected playerFilter(player: Player, opts: object) {
+        if (player.id === 0) return false
+        for (const [key, filterValue] of Object.entries(opts)) {
+            if ((player as any)[key] === filterValue)
+                return false
+        }
+        return true
+    }
+
+    private wrapPlayer(p: NativePlayer): Player {
+        const player = this.players[p.id] || (this.players[p.id] = new Player(p))
+        return Object.assign(player, p)
+    }
+
+    /*
+     * Overridden original methods
+     */
+
+    getPlayer(id: number) {
+        const p = this.room.getPlayer(id)
+        return p === null ? null : this.wrapPlayer(p)
+    }
+
+    getPlayerList(this: DelegatedRoom, teamsOrder: [TeamID, TeamID?, TeamID?] | object | undefined = {}, opts: object = {}) {
+        if (!Array.isArray(teamsOrder)) {
+            opts = teamsOrder
+            teamsOrder = undefined
+        }
+
+        const players: Player[] = []
+        for (let p of this.room.getPlayerList()) {
+            p = this.wrapPlayer(p)
+            if (this.playerFilter(p, opts))
+                players.push(p)
+        }
+
+        // Return just array of players.
+        if (typeof teamsOrder === 'undefined') return players
+
+        // Return array of teams.
+        const teams: [Player[], Player[], Player[]] = [[], [], []]
+        for (let p of players) {
+            // Get team index of returning array.
+            let index = teamsOrder.indexOf(p.team)
+            // If no order index is given, append team to the end.
+            if (index === -1) index = teamsOrder.length
+            teams[index].push(p)
+        }
+        return teams
+    }
+
+    /*
+     * Original methods
+     */
+
+    sendChat(message: string, playerId?: number): void { return this.room.sendChat(message, playerId) }
+    setPlayerAdmin(playerId: number, admin: boolean): void { return this.room.setPlayerAdmin(playerId, admin) }
+    setPlayerTeam(playerId: number, team: number): void { return this.room.setPlayerTeam(playerId, team) }
+    kickPlayer(playerId: number, reason: string = '', ban: boolean = false): void { return this.room.kickPlayer(playerId, reason, ban) }
+    clearBan(playerId: number): void { return this.room.clearBan(playerId) }
+    clearBans(): void { return this.room.clearBans() }
+    setScoreLimit(limit: number): void { return this.room.setScoreLimit(limit) }
+    setTimeLimit(limitInMinutes: number): void { return this.room.setTimeLimit(limitInMinutes) }
+    setCustomStadium(stadiumFileContents: string): void { return this.room.setCustomStadium(stadiumFileContents) }
+    setDefaultStadium(stadiumName: string): void { return this.room.setDefaultStadium(stadiumName) }
+    setTeamsLock(locked: boolean): void { return this.room.setTeamsLock(locked) }
+    setTeamColors(team: TeamID, angle: number, textColor: number, colors: [number, number?, number?]): void { return this.room.setTeamColors(team, angle, textColor, colors) }
+    startGame(): void { return this.room.startGame() }
+    stopGame(): void { return this.room.stopGame() }
+    pauseGame(pause: boolean): void { return this.room.pauseGame(pause) }
+    getScores(): Scores | null { return this.room.getScores() }
+    getBallPosition(): Vector | null { return this.room.getBallPosition() }
+    startRecording(): void { return this.room.startRecording() }
+    stopRecording(): Uint8Array | null { return this.room.stopRecording() }
+    setPassword(password: string): void { return this.room.setPassword(password) }
+}
--- src-ts/declarations/global.d.ts
+++ src-ts/declarations/global.d.ts
@@ -2,44 +2,16 @@ import { NativePlayer } from '../interfaces/NativePlayer';
 import { RoomConfig } from '../interfaces/RoomConfig';
 import { Scores } from '../interfaces/Scores';
 import { TeamID } from '../interfaces/TeamID';
-import { Vector } from '../interfaces/Vector';
 
 
 declare global {
     interface Window {
-        HBInit: typeof HBInit
+        HBInit(config: RoomConfig): any
         onHBLoaded(): void
     }
 }
 
-declare class HBInit {
-    constructor(config: RoomConfig)
-
-
-    sendChat(message: string, playerId?: number): void
-    setPlayerAdmin(playerId: number, admin: boolean): void
-    setPlayerTeam(playerId: number, team: number): void
-    kickPlayer(playerId: number, reason: string, ban: boolean): void
-    clearBan(playerId: number): void
-    clearBans(): void
-    setScoreLimit(limit: number): void
-    setTimeLimit(limitInMinutes: number): void
-    setCustomStadium(stadiumFileContents: string): void
-    setDefaultStadium(stadiumName: string): void
-    setTeamsLock(locked: boolean): void
-    setTeamColors(team: TeamID, angle: number, textColor: number, colors: [number, number?, number?]): void
-    startGame(): void
-    stopGame(): void
-    pauseGame(pause: boolean): void
-    getPlayer(playerId: number): NativePlayer | null
-    getPlayerList(): NativePlayer[]
-    getScores(): Scores | null
-    getBallPosition(): Vector | null
-    startRecording(): void
-    stopRecording(): Uint8Array | null
-    setPassword(password: string): void
-
-
+declare interface RoomObject {
     onPlayerJoin(player: NativePlayer): void
     onPlayerLeave(player: NativePlayer): void
     onTeamVictory(scores: Scores): void
@@ -47,10 +19,10 @@ declare class HBInit {
     onPlayerBallKick(player: NativePlayer): void
     onTeamGoal(team: TeamID): void
     onGameStart(byPlayer: NativePlayer): void
-    onGameStop(byPlayer: NativePlayer): void
+    onGameStop(byPlayer: NativePlayer | null): void
     onPlayerAdminChange(player: NativePlayer, byPlayer: NativePlayer): void
-    onPlayerTeamChange(player: NativePlayer, byPlayer: NativePlayer): void
-    onPlayerKicked(player: NativePlayer, reason: string, ban: boolean, byPlayer: NativePlayer): void
+    onPlayerTeamChange(player: NativePlayer, byPlayer: NativePlayer | null): void
+    onPlayerKicked(player: NativePlayer, reason: string, ban: boolean, byPlayer: NativePlayer | null): void
     onGameTick(): void
     onGamePause(byPlayer: NativePlayer): void
     onGameUnpause(byPlayer: NativePlayer): void
