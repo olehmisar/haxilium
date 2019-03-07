b58d1a8447169cfed1eb0c8d67e9e890f53c191b
olegmisar <olegmisar@gmail.com>
2019-03-07 22:39:21 +0200

    Fix HBInit declaration

--- src-ts/declarations/global.d.ts
+++ src-ts/declarations/global.d.ts
@@ -3,7 +3,7 @@ import { RoomConfig } from '../interfaces/RoomConfig';
 
 declare global {
     interface Window {
-        HBInit(config: RoomConfig): any
+        HBInit(config: RoomConfig<any>): any
         onHBLoaded(): void
     }
 }
