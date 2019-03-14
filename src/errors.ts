cf13a239779ec1cc7529a8627b48410393898ff9
olegmisar <olegmisar@gmail.com>
2019-03-14 16:06:06 +0200

    Fix error classes

--- src/errors.ts
+++ src/errors.ts
@@ -1,16 +1,20 @@
-export class UnknownCommandError extends Error {
+class MyError {
+    constructor(public message: string) { }
+}
+
+export class UnknownCommandError extends MyError {
     constructor(commandName: string) {
         super(`Unknown command ${commandName}`)
     }
 }
 
-export class AccessToCommandDeniedError extends Error {
+export class AccessToCommandDeniedError extends MyError {
     constructor(commandName: string) {
         super(`You don't have access to ${commandName} command`)
     }
 }
 
-export class BadAccessStringError extends Error {
+export class BadAccessStringError extends MyError {
     constructor(accessString: string, reason: string) {
         super(`Bad access string "${accessString}". ${reason}`)
     }
